c2ea09b1bac75a81d0decc322c587d8ad01b4c8f
Omar Zouai <omar@omarzouai.com>
2017-12-10 21:02:14 -0800

    Added Modifying of endpoint targets

---
+++ admin-assets/components/EndpointStatView.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Endpoint from "./Endpoint";
+import {Link} from "react-router-dom";
+import axios from 'axios';
+import DefaultLayout from "./DefaultLayout";
+
+export default class EndpointStatView extends React.Component<{},{}> {
+
+}
